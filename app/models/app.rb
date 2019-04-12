e408d9ee718d57dc0d1d7b64140421181eb30de1
Gui <glmilanez@gmail.com>
2019-04-12 10:38:57 -0400

    submit testing

--- app/models/app.rb
+++ app/models/app.rb
@@ -20,13 +20,4 @@ class App < ApplicationRecord
     where("name >= ? and name <= ?",
     start_name, end_name)
   }
-
-  scope :starts_with_name,   ->  (start_name){
-    where("name >= ?",
-    start_name)
-  }
-  scope :ends_with_name,   ->  (end_name){
-    where("name <= ?",
-    end_name)
-  }
 end
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -17,3 +17,63 @@ RSpec.describe AppsController, type: :controller do
 
   end
 end
+
+RSpec.describe App, type: :model do
+  describe ".between" do
+ 
+      start_id = App.create(name: "my-app-001")
+      mid_id = App.create(name: "my-app-002")
+    end_id = App.create(name: "my-app-003")
+    outside_id = App.create(name: "my-app-004")
+    it "returns only ids between value" do
+      arr = App.between(1, 3)
+      expect(arr[1].name).to include("002")
+    end
+  end
+
+end
+
+RSpec.describe App, type: :model do
+  describe ".starts_with" do
+ 
+      first_id = App.create(name: "my-app-001")
+      second_id = App.create(name: "my-app-002")
+    third_id = App.create(name: "my-app-003")
+    fourth_id = App.create(name: "my-app-004")
+    it "returns only ids after value" do
+      arr = App.starts_with(2)
+      expect(arr[2].name).to include("004")
+    end
+  end
+
+end
+
+RSpec.describe App, type: :model do
+  describe ".ends_with" do
+ 
+      first_id = App.create(name: "my-app-001")
+      second_id = App.create(name: "my-app-002")
+    third_id = App.create(name: "my-app-003")
+    fourth_id = App.create(name: "my-app-004")
+    it "returns only ids before value" do
+      arr = App.ends_with(3)
+      expect(arr[2].name).to include("003")
+    end
+  end
+
+end
+
+RSpec.describe App, type: :model do
+  describe ".between_name" do
+ 
+      first_id = App.create(name: "my-app-001")
+      second_id = App.create(name: "my-app-002")
+    third_id = App.create(name: "my-app-003")
+    fourth_id = App.create(name: "my-app-004")
+    it "returns only names in between value" do
+      arr = App.between_name("my-app-002", "my-app-004")
+      expect(arr[1].name).to include("003")
+    end
+  end
+
+end
